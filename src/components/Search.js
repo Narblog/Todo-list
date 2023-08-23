a8c2322f8681551b5acf98c1ec3c82ec1ccf443e
Narek Grigoryan <ngrigoryan228@gmail.com>
2023-08-23 21:33:21 +0400

    update search.js

---
+++ src/components/Search.js
@@ -0,0 +1,8 @@
+const Search=()=>{
+    return <input type="text" placeholder='Type text for search...'/>
+  
+  
+  }
+
+
+  export default Search
---
+++ src/components/TodoList.js
@@ -0,0 +1,9 @@
+
+const TodoList=()=>{
+ return (<ul>
+    <li>Learn JS</li>
+    <li>Learn React</li>
+  </ul>)
+}
+
+export default TodoList
