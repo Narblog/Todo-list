a7b8a9b92b4aa8bbd8ea09fa2eb696eaa847f0e4
Narek Grigoryan <ngrigoryan228@gmail.com>
2023-09-02 18:35:32 +0400

    update Search and TodoListItem class

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,21 @@
+import { Component } from 'react';
 import './search.css';
 
+
+class Search extends Component{
+  render(){
+    return (
+      <div className='search'>
+        <input type="text" placeholder="Type text for search..." />
+        <button className='search-btn-all'>All</button>
+        <button className='search-btn-done'>Done</button>
+        <button className='search-btn-important'>Important</button>
+      </div>
+    );
+  }
+}
+
+/*
 const Search = () => {
   return (
     <div className='search'>
@@ -9,6 +25,6 @@ const Search = () => {
       <button className='search-btn-important'>Important</button>
     </div>
   );
-}
+}*/
 
 export default Search;
--- src/components/TodoList/TodoListItem/TodoListItem.js
+++ src/components/TodoList/TodoListItem/TodoListItem.js
@@ -1,7 +1,11 @@
-import './todo-list-item.css';
+import { Component } from 'react';
 import {FaTrash,FaInfo,FaCheck} from 'react-icons/fa6'
+import './todo-list-item.css';
 
-const TodoListItem = ({ text, important }) => {
+class TodoListItem extends Component{
+ 
+render(){
+  const{text,important}=this.props
   const style = {
     color: important ? "#d72020" : "black",
     fontWeight:important ? "bold":"normal" 
@@ -20,5 +24,27 @@ const TodoListItem = ({ text, important }) => {
     </li>
   );
 }
+}
+
+/*
+const TodoListItem = ({ text, important }) => {
+  const style = {
+    color: important ? "#d72020" : "black",
+    fontWeight:important ? "bold":"normal" 
+  }
+
+  return (
+    <li className='list-item' style={style}>
+      <span className='item-text'>  {text}</span>
+    
+
+      <span className='item-btns'>
+        <button className='item-btn-done'><FaCheck/></button>
+        <button className='item-btn-important'><FaInfo/></button>
+        <button className='item-btn-remove'><FaTrash/></button>
+      </span>
+    </li>
+  );
+}*/
   
 export default TodoListItem;
